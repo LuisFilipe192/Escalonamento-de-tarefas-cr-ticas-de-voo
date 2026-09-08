0cb1aa59adca1dd0a642c67b73478035a340d05b
LuisFilipe192 <luisfelipe192ss@gmail.com>
2026-09-08 11:55:02 -0300

    valida algoritmo de escalonamento

--- src/scheduler.c
+++ src/scheduler.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct task{
     char nome[50];
@@ -7,6 +9,27 @@ typedef struct task{
     int burst;
 }task;
 
-int main(void) {
+int main(int argc, char *argv[]){
+    FILE *arquivo;
+
+    if(argc != 3){
+        fprintf(stderr,"Erro: número incorreto de argumentos.\n");
+        return 1;
+    }
+
+    if (strcmp(argv[1],"rate") != 0 && strcmp(argv[1],"edf") != 0) {
+        fprintf(stderr,"Erro: algoritmo inválido.\n");
+        return 1;
+    }
+
+    arquivo = fopen(argv[2],"r");
+
+    if(arquivo == NULL){
+        fprintf(stderr,"Erro: não foi possível abrir o arquivo.\n");
+        return 1;
+    }
+
+    fclose(arquivo);
+
     return 0;
 }
