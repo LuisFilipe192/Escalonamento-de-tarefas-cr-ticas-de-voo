0ec64f6ef8d0b911577f408f59007798545748af
LuisFilipe192 <luisfelipe192ss@gmail.com>
2026-09-08 12:35:25 -0300

    le e valida tasks

--- src/scheduler.c
+++ src/scheduler.c
@@ -7,9 +7,12 @@ typedef struct task{
     int periodo;
     int deadline;
     int burst;
+    struct task *next;
 }task;
 
 int main(int argc, char *argv[]){
+    task *head = NULL;
+
     int tempo_total;
 
     FILE *arquivo;
@@ -40,12 +43,47 @@ int main(int argc, char *argv[]){
         fprintf(stderr,"tempo total deve ser positivo.\n");
         return 1;
     }
-    else{
 
+
+    task *novo = (task*)malloc(sizeof(task));
+
+    if(novo == NULL){
+        fprintf(stderr, "Erro: falha ao alocar memória.\n");
+        fclose(arquivo);
+        return 1;
     }
 
+    if (fscanf(arquivo,"%49s %d %d %d", novo->nome, &novo->periodo,&novo->deadline,&novo->burst) != 4){
+        fprintf(stderr,"Erro: arquivo com argumentos inválidos.\n");
+        fclose(arquivo);
+        free(novo);
+        return 1;
+    }
 
-    
+    if(novo->periodo <= 0 || novo->deadline <= 0 || novo->burst <= 0){
+        fprintf(stderr,"Erro: os valores da tarefa devem ser positivos.\n");
+        free(novo);
+        fclose(arquivo);
+        return 1;
+    }
+
+    if(novo->burst > novo->deadline){
+        fprintf(stderr,"Erro: burst não pode ser maior que o deadline.\n");
+        free(novo);
+        fclose(arquivo);
+        return 1;
+    }
+
+    if(novo->deadline > novo->periodo){
+        fprintf(stderr,"Erro: deadline não pode ser maior que o período.\n");
+        free(novo);
+        fclose(arquivo);
+        return 1;
+    }
+
+    novo->next = NULL;
+    head = novo;
+
 
     fclose(arquivo);
 
